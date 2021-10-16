c39ce170c117bf1dbb5cdd5c8db946153ef40c54
hayazaki <toru.hayazaki@gmail.com>
2021-10-16 10:48:26 +0900

    Initial Commit

---
+++ kanij-read/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kanij-read
+//
+//  Created by haayzaki on 2021/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kanij-read/kanij_readApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanij_readApp.swift
+//  kanij-read
+//
+//  Created by haayzaki on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct kanij_readApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
